7d45151639fa07994a122a42da6784c9962802b6
Dani P.M <daniel.paezmartin@gmail.com>
2023-05-02 18:37:52 +0200

    Minor fixes to loading method

--- classification.py
+++ classification.py
@@ -1,11 +1,13 @@
 from ultralytics import YOLO
 
 class Classifier:
-    def __init__(self, pre_train = False, format = 'pt'):
-        if pre_train:
+    def __init__(self, train_method = 'own', format = 'pt'):
+        if train_method == 'own':
             self.weights = 'weights/best_run.' + format # Trained network
+        elif train_method == 'coco':
+            self.weights = 'weights/coco.' + format # Coco trained
         else:
-            self.weights = 'weights/yolo8n.' + format
+            self.weights = 'weights/yolo8n.' + format # Standard weights for detection training
 
         self.model = YOLO(self.weights, task='detect')
 
